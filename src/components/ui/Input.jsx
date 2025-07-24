f2a58fe4f3032d75cd9ec24b6a20bdae21a56fd1
GURUGASTGAR <gurugastgar@gmail.com>
2025-07-24 14:09:21 +0530

    added authpages

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,11 +1,17 @@
 
 
 
-const Input = ({onChange, placeholder,type,value,label}) => {
+const Input = ({onChange, placeholder,type,value,label,name}) => {
   return (
     <div>
-        <label className='mt-5 block text-sm font-medium mb-2' htmlFor='password'>{label}</label>
-        <input type={type} value={value} onChange={onChange} placeholder={placeholder} className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        <label className='mt-2 block text-sm font-medium mb-2' htmlFor='password'>{label}</label>
+        <input 
+              type={type}
+              name={name || undefined}
+               value={value}
+                onChange={onChange}
+                 placeholder={placeholder}
+                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
     </div>
   )
 }
