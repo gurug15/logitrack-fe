f74a36f5266ff7403fb214912253c8a6fc07f452
GURUGASTGAR <gurugastgar@gmail.com>
2025-08-01 18:46:03 +0530

    formated code

--- src/hooks/orders/useGetOrders.js
+++ src/hooks/orders/useGetOrders.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import api from '../../../api/axios';
+import api from '../../api/axios';
 
 export const useGetOrders = () => {
     const [orders, setOrders] = useState([]);
